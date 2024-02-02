6f5859eb86cdf19eac4f11499c8e35b15abd2bf2
TigerBots <118773412+TigerBotsCSP@users.noreply.github.com>
2024-02-02 15:23:33 -0500

    Fix pathplanner bug and remove old paths

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -29,9 +28,6 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-  // Temp: auto selection here
-  SendableChooser<String> chooser = new SendableChooser<>();
-
   /**
    * This function is run when the robot is first started up and should be used
    * for any
@@ -39,13 +35,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Temp: auto selection here
-    chooser.addOption("Balance", "paths/Balance.wpilib.json");
-    chooser.addOption("Three Cubes", "paths/ThreeCubes.wpilib.json");
-    chooser.addOption("Two Cubes", "paths/TwoCubes.wpilib.json");
-    chooser.addOption("Community", "paths/Community.wpilib.json");
-    SmartDashboard.putData("Auto Path", chooser);
-
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
