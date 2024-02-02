add84431dfb79a63a575734460c475938b6271a9
TigerBots <118773412+TigerBotsCSP@users.noreply.github.com>
2024-02-02 15:37:00 -0500

    chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -164,7 +164,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return new PathPlannerAuto("ForwardArc");
-   //return autoChooser.getSelected();
+   return autoChooser.getSelected();
   }
 }
